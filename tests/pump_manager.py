1b632561defbc026c0430b3f146687aec959d7da
sdw-sh <sdw.sh@pm.me>
2023-12-07 20:36:08 +0100

    Black format PumpManager

--- tests/pump_manager.py
+++ tests/pump_manager.py
@@ -6,31 +6,37 @@ import time
 
 logger = logging.getLogger(__name__)
 
+
 class PumpManager:
-    
     critical_time_span_in_s = 4
 
-    def __init__(self, pump_pin: int):
+    def __init__(self, pump_pin: int, on_on_callback=None, on_off_callback=None):
         self.pump_pin = pump_pin
         self.is_locked = False
+        self.on_on_callback = on_on_callback
+        self.on_off_callback = on_off_callback
         # @see https://raspi.tv/2015/rpi-gpio-function-gpio-getmodev2
-        if (GPIO.getmode() != GPIO.BOARD):
-            logger.critical("GPIO board mode not set, board not initiated, terminating");
+        if GPIO.getmode() != GPIO.BOARD:
+            logger.critical("GPIO board mode not set, board not initiated, terminating")
             sys.exit(-1)
-        GPIO.setup(self.pump_pin, GPIO.OUT, initial = GPIO.LOW)
+        GPIO.setup(self.pump_pin, GPIO.OUT, initial=GPIO.LOW)
         logger.info(f"Instantiated  PumpManager (pin {self.pump_pin})")
-        
-        # set up pin
-    
-    def on(self, seconds : float = 0, force : bool = False):
-        if (self.is_locked):
-            logger.warning(f"PumpManager.on called on locked pump (pin {self.pump_pin})")
+
+    def on(self, seconds: float = 0, force: bool = False):
+        if self.is_locked:
+            logger.warning(
+                f"PumpManager.on called on locked pump (pin {self.pump_pin})"
+            )
             self.off()
             return
-        if (seconds > PumpManager.critical_time_span_in_s and force == False):
-            logger.warning(f"To start the pump (pin {self.pump_pin}) with a longer runtime than {PumpManager.critical_time_span_in_s} set the force param to true")
-            return;
+        if seconds > PumpManager.critical_time_span_in_s and force == False:
+            logger.warning(
+                f"To start the pump (pin {self.pump_pin}) with a longer runtime than {PumpManager.critical_time_span_in_s} set the force param to true"
+            )
+            return
         logger.info(f"Starting pump (pin {self.pump_pin}) for {seconds} s")
+        if callable(self.on_on_callback):
+            self.on_on_callback()
         GPIO.output(self.pump_pin, GPIO.HIGH)
         time.sleep(seconds)
         self.off()
@@ -41,6 +47,8 @@ class PumpManager:
 
     def lock(self):
         self.is_locked = True
+        if callable(self.on_off_callback):
+            self.on_off_callback()
         self.off()
         logger.warning(f"Pump (pin {self.pump_pin}) is locked")
 
