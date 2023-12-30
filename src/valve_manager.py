91669deaaadd90b1bf956b147bf4d1e70765e56d
sdw-sh <sdw.sh@pm.me>
2023-12-30 23:15:58 +0100

    Use dict for valve manager instantiation

--- src/valve_manager.py
+++ src/valve_manager.py
@@ -13,32 +13,18 @@ logger = logging.getLogger(__name__)
 class ValveManager:
     def __init__(
         self,
-        valve_0_pin: int,
-        valve_1_pin: int,
-        # valve_2_pin: int,
-        # valve_3_pin: int,
+        valve_dict,
     ):
-        self.valve_0_pin = valve_0_pin
-        self.valve_1_pin = valve_1_pin
-        # self.valve_2_pin = valve_2_pin
-        # self.valve_3_pin = valve_3_pin
+        self.valve_dict = valve_dict
         if GPIO.getmode() != GPIO.BOARD:
             logger.critical("GPIO board mode not set, board not initiated, terminating")
             sys.exit(-1)
-        GPIO.setup(self.valve_0_pin, GPIO.OUT, initial=GPIO.LOW)
-        GPIO.setup(self.valve_1_pin, GPIO.OUT, initial=GPIO.LOW)
-        logger.info(
-            f"Instantiated  ValveManager (pins {self.valve_0_pin}, {self.valve_1_pin})"
-        )
+        for key, value in valve_dict.items():
+            GPIO.setup(value, GPIO.OUT, initial=GPIO.LOW)
+        logger.info(f"Instantiated  ValveManager (valve_dict {valve_dict})")
 
-    def open(self):
-        GPIO.output(self.valve_0_pin, GPIO.HIGH)
+    def open(self, valve_key):
+        GPIO.output(self.valve_dict[valve_key], GPIO.HIGH)
 
-    def close(self):
-        GPIO.output(self.valve_0_pin, GPIO.LOW)
-
-    def open1(self):
-        GPIO.output(self.valve_1_pin, GPIO.HIGH)
-
-    def close1(self):
-        GPIO.output(self.valve_1_pin, GPIO.LOW)
+    def close(self, valve_key):
+        GPIO.output(self.valve_dict[valve_key], GPIO.LOW)
