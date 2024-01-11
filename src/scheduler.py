37221314c35228a1ab709ed4203152c2da527d4a
sdw-sh <152737347+sdw-sh@users.noreply.github.com>
2024-01-11 20:39:01 +0100

    Concept

--- src/scheduler.py
+++ src/scheduler.py
@@ -8,9 +8,11 @@ from event_signal import EventSignal
 class Scheduler:
     def __init__(self) -> None:
         # how often the moisture of the plants should be measured
-        self.moisture_measurement_interval = 60 * 0.00010
+        self.moisture_measurement_interval = 60 * 5 # min
 
+    # TODO exchange with RepeatTimer from https://stackoverflow.com/a/48741004 
     def start(self):
+        self.request_moisture_measurement()
         threading.Timer(
             self.moisture_measurement_interval,
             self.request_moisture_measurement,
