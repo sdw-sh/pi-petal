110c7e6ada45bae32ecb59977452cd5d06d859cf
sdw-sh <152737347+sdw-sh@users.noreply.github.com>
2024-01-15 22:22:50 +0100

    Add possible solution for signal event combination

--- src/event_signal.py
+++ src/event_signal.py
@@ -1,5 +1,12 @@
+# Ponder putting signal and event creator together
+# FOO = ("FOO_SIGNAL", Foo) # where Foo is the event creator class definition (not an instance)
+# may use an named tuple with signal and event
+
 class EventSignal:
     CHECK_PLANTS = "CHECK_PLANTS"
     REQUEST_MOISTURE_MEASUREMENTS = "REQUEST_MOISTURE_MEASUREMENTS"
     MOISTURE_MEASUREMENT_RESULTS = "MOISTURE_MEASUREMENT_RESULTS"
     UPDATE_DISPLAY = "UPDATE_DISPLAY"
+    IRRIGATE = "IRRIGATE"
+    PANIC = "PANIC"
+    CALM = "CALM"
