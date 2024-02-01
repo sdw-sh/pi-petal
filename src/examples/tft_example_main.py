992c6776be6b7c004e8a06bcb49b8ff5249277bc
sdw-sh <sdw.sh@pm.me>
2024-02-01 20:16:11 +0100

    Add TFT display example

--- src/examples/tft_example_main.py
+++ src/examples/tft_example_main.py
@@ -1,3 +1,5 @@
+import time
+
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
@@ -17,31 +19,120 @@ disp = st7735.ST7735(
     offset_left=0,
     offset_top=0,
 )
-WIDTH = disp.width
-HEIGHT = disp.height
-img = Image.new("RGB", (WIDTH, HEIGHT))
+
+disp.begin()
+
+width = disp.width
+height = disp.height
+
+
+if 0:
+    image = Image.open("deployrainbows.gif")
+    frame = 0
+    while True:
+        try:
+            image.seek(frame)
+            disp.display(image.resize((width, height)))
+            frame += 1
+            time.sleep(0.05)
+        except EOFError:
+            frame = 0
+
+
+img = Image.new("RGB", (width, height))
 draw = ImageDraw.Draw(img)
-# Load default font.
-font = ImageFont.load_default(16)
-# font2 = ImageFont.
+
+# Draw a purple rectangle with yellow outline.
+# draw.rectangle(
+#    (10, 10, width - 10, height - 10), outline=(255, 255, 0), fill=(255, 0, 255)
+# )
+
+# Draw some shapes.
+# Draw a blue ellipse with a green outline.
+# draw.ellipse((10, 10, width - 10, height - 10), outline=(0, 255, 0), fill=(0, 0, 255))
+
+line_height = 18
+
+
+# Draw a white X.
+draw.line(
+    (0, line_height, width, line_height),
+    fill=(0, 200, 0),
+)
+# draw.line((10, height - 10, width - 10, 10), fill=(255, 255, 255))
+
+# Draw a cyan triangle with a black outline.
+# draw.polygon(
+#    [(width / 2, 10), (width - 10, height - 10), (10, height - 10)],
+#    outline=(0, 0, 0),
+#    fill=(0, 255, 255),
+# )
+
+
+font_montserrat = ImageFont.truetype("fonts/Montserrat-Bold.ttf", 14)
+font_b612_b = ImageFont.truetype("fonts/B612-Bold.ttf", 14)
+font_b612_r = ImageFont.truetype("fonts/B612-Regular.ttf", 14)
+
+
 # Write some text
 draw.text(
     (2, 0),
     "Hello World! 123456789012345678901234567890",
-    font=font,
+    font=font_montserrat,
     fill=(0, 255, 0),
 )
 draw.text(
-    (2, 18),
+    (2, line_height),
     "Hello World! 123456789012345678901234567890",
-    font=font,
-    fill=(0, 0, 255),
+    font=font_b612_r,
+    fill=(0, 255, 0),
 )
 draw.text(
-    (2, 36),
-    "Special Characters: öäüßáà",
-    font=font,
-    fill=(255, 0, 0),
+    (2, line_height * 2),
+    "Hello World! 123456789012345678901234567890",
+    font=font_b612_b,
+    fill=(0, 255, 0),
+)
+draw.text(
+    (2, line_height * 3),
+    "Hello World! 123456789012345678901234567890",
+    font=font_montserrat,
+    fill=(255, 0, 255),
+)
+draw.text(
+    (2, line_height * 4),
+    "Hello World! 123456789012345678901234567890",
+    font=font_montserrat,
+    fill=(0, 255, 255),
+)
+draw.text(
+    (2, line_height * 5),
+    "Special Characters:",
+    font=font_montserrat,
+    fill=(255, 255, 0),
+)
+draw.text(
+    (2, line_height * 6),
+    "öäüßáà",
+    font=font_b612_r,
+    fill=(255, 255, 255),
 )
 # display!
 disp.display(img)
+
+i = 0
+
+while True:
+    i += 1
+    draw.rectangle(
+        (0, 0, disp.width, disp.height),
+        # outline=(255, 255, 0),
+        fill=(0, 0, 0),
+    )
+    draw.text(
+        (2, line_height * 6),
+        str(i),
+        font=font_b612_r,
+        fill=(255, 255, 255),
+    )
+    disp.display(img)
